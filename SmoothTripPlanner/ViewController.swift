eb300f08f7adcf916e7ba2fdefe978fe0211aee4
YoonhoJo99 <we424239@gmail.com>
2024-06-02 23:21:55 +0900

    .

---
+++ SmoothTripPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmoothTripPlanner
+//
+//  Created by 조윤호 on 6/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
